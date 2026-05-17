cc3ea4caa9c0851e619ab1de5a430cdfa2e23f74
valeriia-kainieva <natureworld95@gmail.com>
2026-05-17 12:27:53 +0300

    gfr-calculator.py

--- gfr-calculator.py
+++ gfr-calculator.py
@@ -11,8 +11,7 @@ if sex not in ['m', 'f']:
 
 try:
     age = float(input('Enter age: '))
-    k_ru = float(input('Serum creatinine (µmol/L): '))
-    
+    k_ru = float(input('Serum creatinine (µmol/L): '))   
 except ValueError:
     print('Invalid input')
     exit()
